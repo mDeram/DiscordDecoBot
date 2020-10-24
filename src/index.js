b937e77bcdc40048bd28f9f77587ef2527f335ca
DeramMath <mathurin9@hotmail.fr>
2020-10-24 15:38:51 +0200

    refactoring & warning fix

--- src/index.js
+++ src/index.js
@@ -3,67 +3,89 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 function min_to_sec(m) { return m*60; }
-function sec_to_milli(s) { return s*1000; }
 function min_to_milli(m) { return sec_to_milli(min_to_sec(m)); }
 
-const disconnect_message = 'You asked for it, see you';
-const warning_time = sec_to_milli(30);
-const force_delay = sec_to_milli(5);
-const force_duration = min_to_milli(1);
-let to_disconnect = [];
+function sec_to_milli(s) { return s*1000; }
+function sec_to_min(s) { return s/60; }
 
-class UserDisconnecting {
+function milli_to_sec(mi) { return mi/1000; }
+function milli_to_min(mi) { return milli_to_sec(sec_to_min(mi)); }
+
+const WARNING_TIME = sec_to_milli(30);
+const FORCE_DELAY = sec_to_milli(5);
+const FORCE_DURATION = min_to_milli(10);
+
+const REPLY_MESSAGE = (minutes) => `I'll disconnect you in ${minutes} minutes`;
+const WARNING_MESSAGE = 'You\'ll be disconnected in ' + milli_to_sec(WARNING_TIME) + ' seconds';
+const DISCONNECT_MESSAGE = 'You asked for it, see you';
+
+let users_to_deco = [];
+
+class UserDeco {
     constructor(msg, time, force, time_stamp) {
         this.msg = msg;
         this.time_to_disconnect = time;
-        this.force = force;
-        this.force_last_connection_time_stamp = time_stamp + time;
+        this.do_force = force;
+        this.force_time_since_last_voice_connection = time_stamp + time;
         this.time_stamp = time_stamp;
         this.timeout = null;
+        users_to_deco.push(this);
     }
+    set_timeout(callback, time) {
+        this.timeout = setTimeout(() => { callback.bind(this)(); }, time);
+    }
+    init() {
+        this.msg.reply(REPLY_MESSAGE(milli_to_min(this.time_to_disconnect)));
+
+        let time_before_warning = this.time_to_disconnect - WARNING_TIME;
+        if (time_before_warning > WARNING_TIME) {
+            this.set_timeout(this.warning, time_before_warning);
+        } else {
+            this.set_timeout(this.deco, this.time_to_disconnect);
+        }
+    }
+    warning() {
+        this.msg.reply(WARNING_MESSAGE);
+
+        this.set_timeout(this.deco, WARNING_TIME);
+    }
+    deco() {
+        this.disconnect();
+
+        if (this.do_force) {
+            this.set_timeout(this.force, FORCE_DELAY);
+        } else {
+            this.destroy();
+        }
+    }
+    force() {
+        let force_stop_time = this.force_time_since_last_voice_connection
+                            + FORCE_DURATION
+                            + FORCE_DELAY;
+
+        if (Date.now() > force_stop_time) {
+            this.destroy();
+        } else {
+            let delay = sec_to_milli(1);
+            if (this.disconnect()) {
+                this.force_time_since_last_voice_connection = Date.now();
+                delay = FORCE_DELAY;
+            }
+            this.set_timeout(this.force, delay);
+        }
+    }
+
     disconnect() {
         if (this.msg.member.voice.channel == null) {
             return false
         };
 
-        this.msg.member.voice.kick(disconnect_message);
+        this.msg.member.voice.kick(DISCONNECT_MESSAGE);
         return true;
     }
     destroy() {
-        to_disconnect.splice(to_disconnect.indexOf(this), 1);
+        users_to_deco.splice(users_to_deco.indexOf(this), 1);
         delete this;
-    }
-}
-
-function force(user_disconnecting) {
-    let force_stop_time = user_disconnecting.force_last_connection_time_stamp
-                        + force_duration
-                        + force_delay;
-                        
-    if (Date.now() > force_stop_time) {
-        user_disconnecting.destroy();
-    } else {
-        let delay = sec_to_milli(1);
-        if (user_disconnecting.disconnect()) {
-            user_disconnecting.force_last_connection_time_stamp = Date.now();
-            delay = force_delay;
-        }
-        user_disconnecting.timeout = setTimeout(force, delay, user_disconnecting);
-    }
-}
-
-function warning(user_disconnecting) {
-    user_disconnecting.msg.reply('attention! Je te déconnecte dans 30 secondes');
-    user_disconnecting.timeout = setTimeout(deco, warning_time, user_disconnecting);
-}
-
-function deco(user_disconnecting) {
-    user_disconnecting.disconnect();
-
-    if (user_disconnecting.force) {
-        user_disconnecting.timeout = setTimeout(force, force_delay, user_disconnecting);
-    } else {
-        user_disconnecting.destroy();
     }
 }
 
@@ -75,15 +97,11 @@ client.on('message', msg => {
     let msg_split = msg.content.split(" ");
     let command = msg_split[0];
     if (command === '!deco') {
-        let force = msg_split[2] === "force";
         let time = Number(msg_split[1]);
-        msg.reply('je te déconnecte dans ' + time + ' minutes');
+        let force = msg_split[2] === "force";
 
-        let time_millisecond = min_to_milli(time);
-
-        let new_user_to_deco = new UserDisconnecting(msg, time, force, Date.now());
-        to_disconnect.push(new_user_to_deco);
-        new_user_to_deco.timeout = setTimeout(warning, time_millisecond - warning_time, new_user_to_deco);
+        let user_to_deco = new UserDeco(msg, min_to_milli(time), force, Date.now());
+        user_to_deco.init();
     }
 });
 
