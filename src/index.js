0900408702c150ba3c5aa49c9297cc5065fe9125
DeramMath <mathurin9@hotmail.fr>
2020-10-24 12:34:59 +0200

    force feature added

--- src/index.js
+++ src/index.js
@@ -2,15 +2,53 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const warning_time = 30*1000;
+function min_to_sec(m) { return m*60; }
+function sec_to_milli(s) { return s*1000; }
+function min_to_milli(m) { return sec_to_milli(min_to_sec(m)); }
+
+const disconnect_message = 'You asked for it, see you';
+const warning_time = sec_to_milli(30);
+const force_delay = sec_to_milli(5);
+const force_duration = min_to_milli(1);
 let to_disconnect = [];
 
 class UserDisconnecting {
-    constructor(msg, time, time_stamp) {
+    constructor(msg, time, force, time_stamp) {
         this.msg = msg;
         this.time_to_disconnect = time;
+        this.force = force;
+        this.force_last_connection_time_stamp = time_stamp + time;
         this.time_stamp = time_stamp;
         this.timeout = null;
+    }
+    disconnect() {
+        if (this.msg.member.voice.channel == null) {
+            return false
+        };
+
+        this.msg.member.voice.kick(disconnect_message);
+        return true;
+    }
+    destroy() {
+        to_disconnect.splice(to_disconnect.indexOf(this), 1);
+        delete this;
+    }
+}
+
+function force(user_disconnecting) {
+    let force_stop_time = user_disconnecting.force_last_connection_time_stamp
+                        + force_duration
+                        + force_delay;
+                        
+    if (Date.now() > force_stop_time) {
+        user_disconnecting.destroy();
+    } else {
+        let delay = sec_to_milli(1);
+        if (user_disconnecting.disconnect()) {
+            user_disconnecting.force_last_connection_time_stamp = Date.now();
+            delay = force_delay;
+        }
+        user_disconnecting.timeout = setTimeout(force, delay, user_disconnecting);
     }
 }
 
@@ -20,9 +58,13 @@ function warning(user_disconnecting) {
 }
 
 function deco(user_disconnecting) {
-    user_disconnecting.msg.member.voice.kick('Bonne nuit');
-    let user = to_disconnect.splice(to_disconnect.indexOf(user_disconnecting), 1);
-    delete user;
+    user_disconnecting.disconnect();
+
+    if (user_disconnecting.force) {
+        user_disconnecting.timeout = setTimeout(force, force_delay, user_disconnecting);
+    } else {
+        user_disconnecting.destroy();
+    }
 }
 
 client.on('ready', () => {
@@ -32,13 +74,14 @@ client.on('ready', () => {
 client.on('message', msg => {
     let msg_split = msg.content.split(" ");
     let command = msg_split[0];
-    let time = Number(msg_split[1]);
     if (command === '!deco') {
+        let force = msg_split[2] === "force";
+        let time = Number(msg_split[1]);
         msg.reply('je te déconnecte dans ' + time + ' minutes');
 
-        let time_millisecond = time*60*1000;
+        let time_millisecond = min_to_milli(time);
 
-        let new_user_to_deco = new UserDisconnecting(msg, time, Date.now());
+        let new_user_to_deco = new UserDisconnecting(msg, time, force, Date.now());
         to_disconnect.push(new_user_to_deco);
         new_user_to_deco.timeout = setTimeout(warning, time_millisecond - warning_time, new_user_to_deco);
     }
